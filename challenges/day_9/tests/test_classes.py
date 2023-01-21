7ad1f6da78c569263dadff1df567de2ad13236b6
ConradBohm <conrad.bohm@hotmail.co.uk>
2023-01-21 02:16:08 +0000

    update tests to reflect code changes

--- challenges/day_9/tests/test_classes.py
+++ challenges/day_9/tests/test_classes.py
@@ -1,36 +1,46 @@
 from classes import *
+from main import setup_rope
 
 up = ['U',1]
 down = ['D',2]
 left = ['L',3]
 right = ['R',4]
 
-def test_update_position():
+def test_update_position_rope_class():
     test_rope = Rope()
+    setup_rope(test_rope)
 
     test_rope.update_position(up)
-    assert test_rope.head_position == [0,1]
+    assert test_rope.nodes[0].position == [0,1]
 
     test_rope.update_position(down)
-    assert test_rope.head_position == [0,-1]
+    assert test_rope.nodes[0].position == [0,-1]
 
     test_rope.update_position(left)
-    assert test_rope.head_position == [-3,-1]
+    assert test_rope.nodes[0].position == [-3,-1]
 
     test_rope.update_position(right)
-    assert test_rope.head_position == [1,-1]
+    assert test_rope.nodes[0].position == [1,-1]
 
-def test_move_tail():
+
+def test_update_position_node_class():
     test_rope = Rope()
+    setup_rope(test_rope)
 
     test_rope.update_position(up) 
-    assert test_rope.tail_position == [0,0]
+    assert test_rope.nodes[1].position == [0,0]
 
     test_rope.update_position(down)
-    assert test_rope.tail_position == [0,0]
+    assert test_rope.nodes[1].position == [0,0]
 
     test_rope.update_position(left)
-    assert test_rope.tail_position == [-2,-1]
+    assert test_rope.nodes[1].position == [-2,-1]
 
     test_rope.update_position(right)
-    assert test_rope.tail_position == [0,-1]
+    assert test_rope.nodes[1].position == [0,-1]
+
+def test_set_parent():
+    pass
+
+def test_set_child():
+    pass
--- challenges/day_9/tests/test_main.py
+++ challenges/day_9/tests/test_main.py
@@ -3,15 +3,15 @@ from main import *
 test_data = open("tests/test_data/test_data.txt", "r")
 test_data = test_data.readlines()
 
-def test_main():
+def test_second_position():
     test_rope = Rope()
 
     for line in test_data:
         single_instruction = line.split()
         test_rope.update_position(single_instruction)
 
-    print(test_rope.tail_tracker)
-    output = set(map(tuple,test_rope.tail_tracker))
+    location_list = test_rope.nodes[1].locaction_tracker
+    output = set(map(tuple,location_list))
     assert len(output) == 13
 
     
