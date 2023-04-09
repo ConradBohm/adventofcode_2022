d63887d1573a845176464628a7c49d382c51b3db
ConradBohm <conrad.bohm@hotmail.co.uk>
2023-04-09 01:37:48 +0100

    remove cpu class copy, and added crt logic

--- challenges/day_10/main.py
+++ challenges/day_10/main.py
@@ -3,45 +3,13 @@ import classes
 instruction_data = open("input.txt", "r")
 instruction_data = instruction_data.readlines()
 
-class CPU:
-    def __init__(self) -> None:
-        self.cycle = 0
-        self.register_x = 1
-        self.current_instruction = []
-        self.instruction_timer = 0
-        self.mode = None
-
-
-    def load_instruction(self, instruction) -> None:
-        new_instruction = instruction.split()
-        self.current_instruction = new_instruction
-
-    def get_signal_strength(self) -> int:
-        return self.cycle * self.register_x
-    
-    def update_register(self) -> None:
-        if self.current_instruction[0][:3] == 'add':
-            if self.current_instruction[0][3] == 'x':
-                self.register_x += int(self.current_instruction[1])
-
-        else:
-            print(f"Error:- inappropriate instruction - {self.current_instruction[0]}")
-
-    def increment_cycle(self) -> None:
-        self.cycle += 1
-
-    def update_mode(self):
-        if self.mode == 'addx':
-            self.mode = None
-        else:
-            self.mode = self.current_instruction[0]
-
-    def produce_output(self):
-        print(f"Signal strength from register X for cycle {self.cycle} is {self.get_signal_strength()}")
-        return int(self.get_signal_strength())
-
-
 cpu = classes.CPU()
+crt = classes.CRT()
+
+def update_cycle():
+    cpu.increment_cycle()
+    crt.increment_cycle()
+    crt.update_position()
 
 def get_latest_instruction(instruction_list):
     output = instruction_list[0]
@@ -52,7 +20,8 @@ def main(instruction_data):
     total_signal_strength_output = 0
     
     while True:
-        cpu.increment_cycle()
+        update_cycle()
+        crt.draw_pixel(cpu)
 
         if cpu.cycle in [20,60,100,140,180,220]:
             total_signal_strength_output += cpu.produce_output()
@@ -67,8 +36,9 @@ def main(instruction_data):
             cpu.update_register()
             cpu.update_mode()
 
-        if cpu.cycle > 220:
+        if len(instruction_data) == 0:
             print(f"Total signal strength from the above breakpoints is {total_signal_strength_output}")
+            crt.render_screen()
             return total_signal_strength_output
 
 if __name__ == "__main__":
